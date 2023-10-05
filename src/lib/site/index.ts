97cae4e703847a2ae7095406f84f6ff9f9a5317b
EdwardR0507 <ramosedward21@gmail.com>
2023-10-05 14:41:19 -0500

    feat: initialize the project

---
+++ src/lib/site/index.ts
@@ -0,0 +1,12 @@
+const BASE_URL = "http://localhost:3000";
+
+export const siteConfig = {
+  name: "Edward Ramos",
+  description:
+    "Hi, welcome to my digital space. Here I share my side projects, tutorials & what I learn over time. I am looking to grow my career in tech 🚀",
+  url: BASE_URL,
+  links: {
+    twitter: "https://twitter.com/EdRamosV",
+    github: "https://github.com/EdwardR0507",
+  },
+};
